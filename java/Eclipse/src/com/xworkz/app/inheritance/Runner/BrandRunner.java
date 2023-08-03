c7b8c67e6df23ec25934430e5a34ad3095a0a535
AhamedMathin <mathinahamed.xworkz@gmail.com>
2023-08-03 23:10:38 +0530

    Inheritance Casting

--- java/Eclipse/src/com/xworkz/app/inheritance/Runner/BrandRunner.java
+++ java/Eclipse/src/com/xworkz/app/inheritance/Runner/BrandRunner.java
@@ -9,24 +9,22 @@ import com.xworkz.app.inheritance.util.BrandUtil;
 public class BrandRunner {
 
 	public static void main(String[] args) {
-		
-	
-			System.out.println("invoking main of BrandRunner");
-			Brand brand =new Brand();
-			BrandUtil.run(brand);
-	       
-	        Brand brand2=new LocalBrand();
-	        LocalBrand localBrand=(LocalBrand) brand2;
-	        BrandUtil.run(localBrand);
-			
-	        Brand brand3=new NationalBrand();
-	        NationalBrand nationalBrand=(NationalBrand) brand3;
-	        BrandUtil.run(nationalBrand);
-	        
-	        Brand brand4=new InternationalBrand();
-	        InternationalBrand internationalBrand=(InternationalBrand) brand4;
-	        BrandUtil.run(internationalBrand);
-		}
-	
+
+		System.out.println("invoking main of BrandRunner");
+		Brand brand = new Brand();
+		BrandUtil.run(brand);
+
+		Brand brand2 = new LocalBrand();
+		LocalBrand localBrand = (LocalBrand) brand2;
+		BrandUtil.run(localBrand);
+
+		Brand brand3 = new NationalBrand();
+		NationalBrand nationalBrand = (NationalBrand) brand3;
+		BrandUtil.run(nationalBrand);
+
+		Brand brand4 = new InternationalBrand();
+		InternationalBrand internationalBrand = (InternationalBrand) brand4;
+		BrandUtil.run(internationalBrand);
+	}
 
 }
